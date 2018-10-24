753ebb6ae44891d234112d6c646483498ababbd6
vagrant <vagrant@vagrant.vm>
2018-10-24 02:39:54 +0000

    feat: add initial code
    This code tests the GET requests useing postman tool.

---
+++ api_server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/readHello')
+def getRequestHello():
+    return "Hi, I got your GET Request!"
+
+
+@app.route('/createHello', methods=['POST'])
+def postRequestHello():
+    return "I see you sent a POST message :-)"
+
+
+@app.route('/updateHello', methods=['PUT'])
+def updateRequestHello():
+    return "Sending Hello on an PUT request!"
+
+
+@app.route('/deleteHello', methods=['DELETE'])
+def deleteRequestHello():
+    return '''Deleting your hard drive.....haha 
+        just kidding! I received a DELETE request!'''
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000) 
